6e08057b8fc0b7da8d9a0a651f8603bf6a26a933
rover-otto-tamas <otto.tamas@rover.com>
2024-04-22 15:03:35 +0000

    Adapt offset pagination tests

--- tests/paginate/test_offset_pagination.py
+++ tests/paginate/test_offset_pagination.py
@@ -10,14 +10,20 @@ def expected_ticket_count(zd: Zendesk):
     return count
 
 
-def test_search_pagination(zd: Zendesk, expected_ticket_count: int):
+def test_search_offset_pagination(zd: Zendesk, expected_ticket_count: int):
     # Try to implement a way to return partial results.
     # This includes refactoring, because results are merged together at the end
     # of pagination.
     try:
-        response = zd.search(query="*", per_page=100, get_all_pages=True)
+        response = zd.search(query="*", get_all_pages=True, cursor_pagination=False)
     except ZendeskError as e:
         assert len(e.partial_results["results"]) == 1000
         assert e.partial_results["count"] == expected_ticket_count
     else:
         assert len(response["results"]) == expected_ticket_count
+
+def test_search_with_cursor_pagination_is_unsupported(zd: Zendesk):
+    with pytest.raises(ZendeskError) as e:
+        zd.search(query="*", get_all_pages=True, cursor_pagination=True)
+    
+    assert e.value.partial_results is None
